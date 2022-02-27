1162fb6724a050fb51ef78ee811c758c688f5f15
Reuben deVries <reubendevries@gmail.com>
2022-02-26 19:45:46 -0800

    added proper naming

---
+++ Hello/src/Hello/Hello.java
@@ -0,0 +1,14 @@
+package Hello.src.Hello;
+
+public class Hello{
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Reuben");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+    }   
+}
